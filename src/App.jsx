110d4517ce87a30adb6736569301731075eb8904
MarkiianPerliak <perliak2012@gmail.com>
2025-10-06 17:42:35 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import {Home} from './pages/Home/Home.jsx';
+
+export const App = () => {
+  return (
+    <div className='App'>
+      <Home/>
+    </div>
+  )
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import {Header} from './components/Header/Header.jsx';
+
+export const Home = () => {
+    return (
+        <div className="Home">
+            <Header/>
+        </div>
+    )
+}
